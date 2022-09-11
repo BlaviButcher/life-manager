a61c312cf74b2a9df4f122e3756f1a0f043fdba2
Riley Douglas <rileydouglas@outlook.com>
2022-09-10 23:11:15 -0500

    fixed typos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,15 +7,15 @@
 		One of the biggest challenges I face with software development is remembering implementations I
 		have previously done. Unfortunately, the brain works on a 'use it or lose it' basis; often I do
 		certain programming tasks only once every few months. I often take notes on concepts I know I
-		will not cover for a while, but often it is not enough.
+		will not cover for a while, but it's usually not enough.
 	</p>
 
 	<p>
 		The idea of this project is to implement different things using svelte and sveltekit that I've
-		touched on at work or projects, creating a pratical wiki of implemetations. Each project or
-		implementation will have it's own tab found on the navbar and below with some addtional
-		information as necessary. It will server as a place for me to return to jog my memory and give
-		myself some working examples on things long forgotten.
+		touched on at work or in personal projects, creating a pratical wiki of implemetations. Each
+		project or implementation will have it's own tab found on the navbar and below with some
+		additional information as necessary. It will serve as a place for me to jog my memory and give
+		myself some working examples on things that are but a vague mirage.
 	</p>
 </page-content>
 
